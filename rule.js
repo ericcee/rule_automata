16923adae51bdd724ae8c955cde1e8e313d5526c
root <root@DESKTOP-2PONAK5.localdomain>
2021-06-03 13:20:35 +0200

    bug fix

--- rule.js
+++ rule.js
@@ -13,7 +13,7 @@ var rule = function(width, height, rules, pxPerW, pxPerH, canvas) {
         pixArray.add(val);
     }
     
-    var intTopointToIntnt = function(x) {
+    var intToPoint = function(x) {
         var y = Math.floor(x/pxPerH);
         var x = Math.floor(x%pxPerW);
         return {x: x, y: y};
@@ -28,7 +28,7 @@ var rule = function(width, height, rules, pxPerW, pxPerH, canvas) {
         ctx.fillStyle = "black";
         
         for (var it = pixArray.values(), val= null; val=it.next().value; ) {
-            var pt = intTopointToIntnt(val);
+            var pt = intToPoint(val);
             ctx.beginPath();
             ctx.rect(pt.x*cw, pt.y*ch, cw, ch);
             ctx.fill();
@@ -37,34 +37,60 @@ var rule = function(width, height, rules, pxPerW, pxPerH, canvas) {
     
     var getNextFrame = function(){
         var y = 0;
-        
+        console.log(pixArray.size);
         for (var it = pixArray.values(), val= null; val=it.next().value; ) {
-            var pp = intTopointToIntnt(val);
+            var pp = intToPoint(val);
             
             if(pp.y + 1 > pxPerH){
                 return false; // Reached the end. Return false.
             }
-            
-            if(Math.max(y, pp.y)!=y) y = pp.y;
+            y = Math.max(y, pp.y);
         }
         
         for(var x = 0; x < pxPerW; x++){
             var c = 0;
             // Get Neighbours left right and middle above the pixel.
-            var b1 = pixArray.has(pointToInt(x-1, y));
-            var b2 = pixArray.has(pointToInt(x, y));
-            var b3 = pixArray.has(pointToInt(x+1, y));
+            var b1 = 0, b2 = 0, b3 = 0;
+            
+            if(x-1 >= 0) b1 = pixArray.has(pointToInt(x-1, y));
+            b2 = pixArray.has(pointToInt(x, y));
+            if(x+1 <= pxPerW-1) b3 = pixArray.has(pointToInt(x+1, y));
+            
             c = b1 | (b2 << 1) | (b3 << 2); // Convert neighbours to index for the given rules.
             if(rules[c]) {
-                pixArray.add(pointToInt(x, y+1)); // Add the pixel if the rules fit.
+                pixArray.add(pointToInt(x, y + 1)); // Add the pixel if the rules fit.
             }
         }
         return true;
     }
     
+    var removeLine = function(line){
+        var temp = new Set();
+        var y = 0;
+        for (var it = pixArray.values(), val= null; val=it.next().value; ) {
+            var pp = intToPoint(val);
+            
+            if(pp.y == line) continue;
+            
+            if(pp.y < line) temp.add(pointToInt(pp.x, pp.y));
+            else if(pp.y > line) temp.add(pointToInt(pp.x, pp.y - 1));
+        }
+        pixArray = temp;
+    }
+    
+    var fillRandomDots = function(size){
+        for(var i = 0; i < size; i++){
+            var b = Math.random()*pxPerH*pxPerW;
+            pixArray.add(b);
+        }
+        
+    }
+    
     this.getNextFrame = getNextFrame;
     this.draw = draw;
     this.setPixel = setPixel;
+    this.removeLine = removeLine;
+    this.fillRandomDots = fillRandomDots;
 }
 
 var canv = document.getElementById("rule");
@@ -72,19 +98,21 @@ var canv = document.getElementById("rule");
 //Rules
 var rule_30 = [false, true, true, false, true, false, true, false];
 var rule_90 = [false, true, false, true, true, false, true, false];
+var rule_110 = [false, false, true, true, true, true, true, false];
 
 var cwidth = canv.width;
 var cheight = canv.height;
 
-var pxPerSide = Math.floor(cwidth);
+var pxPerSide = Math.floor(512);
 
 //Initiate rule class
 var r = new rule(cwidth, cheight, rule_30, pxPerSide, pxPerSide, canv);
 
-r.setPixel(Math.floor(pxPerSide/2), 0);
+r.setPixel(Math.floor((pxPerSide-1)/2), 0);
 
 //Itterate all frames
 while(r.getNextFrame());
 
 //Draw to canvas
 r.draw();
+
